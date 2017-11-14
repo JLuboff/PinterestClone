f5e17b6ec91827321cf860198b8abeb299b54af0
Jason L <jluboff@gmail.com>
2017-11-14 14:57:09 -0800

    Cleaned up code for route and app files, deploying to glitch

--- app.js
+++ app.js
@@ -6,48 +6,52 @@ const express = require('express'),
       session = require('express-session'),
       MongoClient = require('mongodb').MongoClient,
       ObjectID = require('mongodb').ObjectID,
-      dotenv = require('dotenv').config(),
       bodyParser = require('body-parser');
       port = process.env.PORT || 3000;
 
 const app = express();
 
-passport.use(new TwitterStrategy({
-  consumerKey: process.env.TWITKEY,
-  consumerSecret: process.env.TWITSECRET,
-  callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
-},
-(token, tokenSecret, profile, cb) => {
-  if(profile){
-    user = profile;
-    return cb(null, user);
-  } else {
-    return done(null, false);
-  };
-}));
+passport.use(
+	new TwitterStrategy(
+		{
+			consumerKey: process.env.TWITKEY,
+			consumerSecret: process.env.TWITSECRET,
+			callbackURL: process.env.CALLBACKURL
+		},
+		(token, tokenSecret, profile, cb) => {
+			if (profile) {
+				user = profile;
+				return cb(null, user);
+			} else {
+				return done(null, false);
+			}
+		}
+	)
+);
 
 passport.serializeUser((user, cb) => {
-  cb(null, user);
+	cb(null, user);
 });
 passport.deserializeUser((obj, cb) => {
-  cb(null, obj);
+	cb(null, obj);
 });
 
 app.set('view engine', 'hbs');
-app.use(session({
-  secret: process.env.SESSSECRET,
-  resave: true,
-  saveUnitialized: true
-}));
+app.use(
+	session({
+		secret: process.env.SESSSECRET,
+		resave: true,
+		saveUnitialized: true
+	})
+);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-MongoClient.connect(`${process.env.MONGOROUTE}`, (err, db) =>{
-
-routes(app, passport, db);
-})
+MongoClient.connect(`${process.env.MONGOROUTE}`, (err, db) => {
+	routes(app, passport, db);
+});
 
 app.listen(port, () => {
-  console.log(`Listening on port: ${port}`);
+	console.log(`Listening on port: ${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,94 +2,133 @@ const bodyParser = require('body-parser'),
       ObjectId = require('mongodb').ObjectId;
 
 module.exports = (app, passport, db) => {
-  const isLogged = (req, res, next) => {
-    if(req.isAuthenticated()) return next();
-    return res.redirect('/login');
-  };
+	const isLogged = (req, res, next) => {
+		if (req.isAuthenticated()) return next();
+		return res.redirect('/login');
+	};
 
-  app.route('/').get((req, res) => {
-    console.log(req.user);
-    let loggedIn = req.user != undefined ? true : false,
-        id = req.user != undefined ? req.user._json.id : undefined;
+	app.route('/').get((req, res) => {
+		let loggedIn = req.user != undefined ? true : false,
+			id = req.user != undefined ? req.user._json.id : undefined;
 
-    db.collection('posts').find().toArray((err, doc) =>{
-      if(err) throw err;
-      res.render('index.hbs', {loggedIn, id, doc});
-    })
-  })
+		db
+			.collection('posts')
+			.find()
+			.toArray((err, doc) => {
+				if (err) throw err;
+				res.render('index.hbs', { loggedIn, id, doc });
+			});
+	});
 
-  app.route('/usersPosts/:id').get(isLogged, (req, res) => {
-    db.collection('posts').find({'post.user': Number(req.params.id)}).toArray((err, data) => {
-      if(err) throw err;
-      res.render('userspost.hbs', {loggedIn: true, data})
-    })
-  })
+	app.route('/auth/twitter/callback').get(
+			passport.authenticate('twitter', { failureRedirect: '/login' }),
+			(req, res) => {
+				res.redirect('/');
+			}
+		);
 
-  app.route('/seeUserPosts/:id').get((req, res) => {
-    let loggedIn = req.user != undefined ? true : false;
-    db.collection('posts').find({'post.user': Number(req.params.id)}).toArray((err, data) => {
-      if(err) throw err;
-      res.render('seeuserposts.hbs', {loggedIn, data})
-    })
-  })
+	app.route('/createPost').post((req, res) => {
+		let post = {
+			user: req.user._json.id,
+			username: req.user._json.screen_name,
+			avatar: req.user._json.profile_image_url_https,
+			likes: 0,
+			likedBy: [],
+			postImage: req.body.imageUrl,
+			postDescription: req.body.description
+		};
 
-  app.route('/deletePost/:id').delete((req, res) => {
-    db.collection('posts').findOneAndDelete({_id: ObjectId(req.params.id)}).then(() => {
-      db.collection('posts').find({'post.user': Number(req.user._json.id)}).toArray((err, data) => {
-        if(err) throw err;
+		db.collection('posts').insertOne({ post });
+		res.redirect(`/usersPosts/${req.user.id}`);
+	});
 
-        res.json(data);
-      })
-    })
-  })
+	app.route('/deletePost/:id').delete((req, res) => {
+		db
+			.collection('posts')
+			.findOneAndDelete({ _id: ObjectId(req.params.id) })
+			.then(() => {
+				db
+					.collection('posts')
+					.find({ 'post.user': Number(req.user._json.id) })
+					.toArray((err, data) => {
+						if (err) throw err;
+						res.json(data);
+					});
+			});
+	});
 
-  app.route('/postCounter/:id').post(isLogged, (req, res) => {
-    console.log(req.params.id);
-    let id = req.params.id;
-    db.collection('posts').aggregate([{$match: {_id: ObjectId(id)}}, {$unwind: '$post.likedBy'}, {$match: {'post.likedBy': req.user._json.id}}], (err, doc) => {
-      if(err) throw err;
-      console.log(doc);
-      if(doc.length){
-        db.collection('posts').findOneAndUpdate({_id: ObjectId(id)}, { $inc: {'post.likes': -1}, $pull: {'post.likedBy' : req.user._json.id}}, {upsert: true, returnOriginal: false}, (err, doc) => {
-          if(err) throw err;
-          console.log('Found: ' + JSON.stringify(doc.value));
-          res.json(doc.value);
-        })
-      } else {
-        db.collection('posts').findOneAndUpdate({_id: ObjectId(id)}, { $inc: {'post.likes': 1}, $addToSet: {'post.likedBy': req.user._json.id}}, {upsert: true, returnOriginal: false}, (err, doc) => {
-          if(err) throw err;
-          console.log('Not Found: ' + JSON.stringify(doc.value));
-          res.json(doc.value);
-        });
-      }
-    })
-  })
+	app.route('/login').get(passport.authenticate('twitter'));
 
-  app.route('/createPost').get(isLogged, (req, res) => {
-    res.render('createpost.hbs');
-  }).post((req, res) => {
-    console.log(req.body);
-    let post = {
-      user: req.user._json.id,
-      username: req.user._json.screen_name,
-      avatar: req.user._json.profile_image_url_https,
-      likes: 0,
-      likedBy: [],
-      postImage: req.body.imageUrl,
-      postDescription: req.body.description
-    };
+	app.route('/logout').get((req, res) => {
+		req.logout();
+		res.redirect('/');
+	});
 
-    db.collection('posts').insertOne({post});
-    res.redirect(`/usersPosts/${req.user.id}`);
-  })
-  app.route('/login').get(passport.authenticate('twitter'));
+	app.route('/postCounter/:id').post(isLogged, (req, res) => {
+		let id = req.params.id;
+		db
+			.collection('posts')
+			.aggregate(
+				[
+					{ $match: { _id: ObjectId(id) } },
+					{ $unwind: '$post.likedBy' },
+					{ $match: { 'post.likedBy': req.user._json.id } }
+				],
+				(err, doc) => {
+					if (err) throw err;
+					if (doc.length) {
+						db
+							.collection('posts')
+							.findOneAndUpdate(
+								{ _id: ObjectId(id) },
+								{
+									$inc: { 'post.likes': -1 },
+									$pull: { 'post.likedBy': req.user._json.id }
+								},
+								{ upsert: true, returnOriginal: false },
+								(err, doc) => {
+									if (err) throw err;
+									res.json(doc.value);
+								}
+							);
+					} else {
+						db
+							.collection('posts')
+							.findOneAndUpdate(
+								{ _id: ObjectId(id) },
+								{
+									$inc: { 'post.likes': 1 },
+									$addToSet: { 'post.likedBy': req.user._json.id }
+								},
+								{ upsert: true, returnOriginal: false },
+								(err, doc) => {
+									if (err) throw err;
+									res.json(doc.value);
+								}
+							);
+					}
+				}
+			);
+	});
 
-  app.route('/logout').get((req, res) => {
-    req.logout();
-    res.redirect('/');
-  });
+	app.route('/seeUserPosts/:id').get((req, res) => {
+		let loggedIn = req.user != undefined ? true : false;
+		db
+			.collection('posts')
+			.find({ 'post.user': Number(req.params.id) })
+			.toArray((err, data) => {
+				if (err) throw err;
+				res.render('seeuserposts.hbs', { loggedIn, data });
+			});
+	});
 
-  app.route('/auth/twitter/callback').get(passport.authenticate('twitter', { failureRedirect: '/login'}), (req, res) => {
-      res.redirect('/');
-    });
-}
+	app.route('/usersPosts/:id').get(isLogged, (req, res) => {
+		db
+			.collection('posts')
+			.find({ 'post.user': Number(req.params.id) })
+			.toArray((err, data) => {
+				if (err) throw err;
+				res.render('userspost.hbs', { loggedIn: true, data });
+			});
+	});
+};
